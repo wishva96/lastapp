fa577c68c4ddfaeb0e44bc4272ac76ea92d00dcc
Shanthalingam Wishvagaran <shanthalingam_2016126@fot.ruh.ac.lk>
2022-06-29 07:55:17 +0530

    UI creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lastapp/screens/tasks_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-        title: Text('To Do List'),
-        backgroundColor: Colors.blueGrey[900] ,
-      ),
-        body: Center(
-          child: Image(
-            image: NetworkImage('https://media.newstrack.in/uploads/other-news/religious-news/Dec/25/big_thumb/asdsda_5e033b16da7e8.jpg'),
-          ),
-        )
-      ),
-    ),
-  );
-}
+
+void main() => runApp(MyApp());
+  class MyApp extends StatelessWidget {
+   Widget build(BuildContext context){
+     return MaterialApp(
+       home: TasksScreen(),
+     );
+
+
+   }
+
+  }
+
